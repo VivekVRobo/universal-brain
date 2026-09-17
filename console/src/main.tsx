6086f417ba692691599b1455e08b11f08f3fc2c7
Vivek Vala <vivekvala562@gmail.com>
2026-09-18 03:01:57 +0530

    security: mount operator auth gate in console

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ConsoleProvider } from "./context/ConsoleContext";
 import { AppContent } from "./App";
+import { OperatorAuthGate } from "./components/OperatorAuthGate";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConsoleProvider>
-      <AppContent />
-    </ConsoleProvider>
+    <OperatorAuthGate>
+      <ConsoleProvider>
+        <AppContent />
+      </ConsoleProvider>
+    </OperatorAuthGate>
   </React.StrictMode>
 );
